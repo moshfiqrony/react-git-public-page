8ac46835c665ed565d46279398ff9baf0e7d1393
moshfiqrony <moshfiqrony@gmail.com>
2019-11-17 15:14:24 +0600

    Updates

---
+++ precache-manifest.db42b8ea50c9a655f5cc20ec27fa368b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8172caa48846baef679e30634ed2bb3",
+    "url": "/react-git-public-page/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-git-public-page/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8588e805042568d8b1a1",
+    "url": "/react-git-public-page/static/js/main.2ef3bc65.chunk.js"
+  },
+  {
+    "revision": "d51df8ddc8647218d686",
+    "url": "/react-git-public-page/static/js/runtime~main.49d50365.js"
+  }
+]);
